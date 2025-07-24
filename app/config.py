bc4031ce8985cbd9a9cf2c9b1053f39e11fcac1d
kadir123456 <47676558+kadir123456@users.noreply.github.com>
2025-07-24 14:35:17 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -15,7 +15,7 @@ class Settings:
 
     # --- İşlem Parametreleri ---
     LEVERAGE: int = 5
-    ORDER_SIZE_USDT: float = 20.0
+    ORDER_SIZE_USDT: float = 100.0
     TIMEFRAME: str = "5m"
     
     # --- Kâr/Zarar Ayarları (Sadece Stop Loss) ---
