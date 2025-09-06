f54bf8f2f2b6fbfc7a761592a4d597ccbeacf1c6
kadir123456 <47676558+kadir123456@users.noreply.github.com>
2025-09-06 15:22:11 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -16,11 +16,11 @@ class Settings:
     # --- İşlem Parametreleri ---
     LEVERAGE: int = 5
     ORDER_SIZE_USDT: float = 40.0
-    TIMEFRAME: str = "15m"
+    TIMEFRAME: str = "30m"
     
     # --- Kâr/Zarar Ayarları (Stop Loss ve Take Profit) ---
-    STOP_LOSS_PERCENT: float = 0.02  # %2 Zarar Durdur
-    TAKE_PROFIT_PERCENT: float = 0.02 # %2 Kar Al
+    STOP_LOSS_PERCENT: float = 0.03  # %2 Zarar Durdur
+    TAKE_PROFIT_PERCENT: float = 0.05 # %2 Kar Al
     COMPOUND_RATIO: float = 0.5 # Elde edilen karin %50'si bir sonraki islem miktarini artirmak icin kullanilir
 
 settings = Settings()
